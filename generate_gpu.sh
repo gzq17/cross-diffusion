12ecc7630cd68d982cb3f290863a5d87ed11690a
夜听残荷 <50518469+gzq17@users.noreply.github.com>
2025-11-18 21:49:44 +0800

    修改指令
    Added installation commands for required packages and updated OPENAI_LOGDIR.

--- generate_gpu.sh
+++ generate_gpu.sh
@@ -2,3 +2,12 @@ export OPENAI_LOGDIR=/data/guozhanqiang/binary2gray/Improve-DDPM
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4
 mpiexec -n 5 python scripts/image_train_3D.py
 mpiexec -n 3 python scripts/image_finetune.py
+
+pip3 install blobfile
+sudo apt-get install openmpi-bin openmpi-dev
+pip3 install mpi4py
+sudo apt-get install openmpi-bin openmpi-common libopenmpi-dev
+pip3 install SimpleITK
+pip3 install scipy
+export OPENAI_LOGDIR=/opt/tiger/gzq_data/Improve-DDPM
+wget .. -O CTA_data.zip
